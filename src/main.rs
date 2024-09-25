40664cd90c5a6a947537ec87bf446e1e1d3dc679
ssamaruk <ssamaruk@magnite.com>
2024-09-25 14:23:25 +0300

    Parens

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 enum Token {
     Literal(String),
     Op(String),
+    OpenParen,
+    ClosedParen
 }
 
 fn tokenize(s: &str) -> Vec<Token> {
@@ -15,11 +17,9 @@ fn tokenize(s: &str) -> Vec<Token> {
                 while let Some(c) = chars.next_if(|c| c.is_digit(10)) { token.push(c); }
                 tokens.push(Token::Literal(token));
             }
-            '+' | '-' | '/' | '%' => {
-                if let Some(c) = chars.next() {
-                    tokens.push(Token::Op(c.to_string()));
-                }
-            }
+            '+' | '-' | '/' | '%' => { if let Some(c) = chars.next() { tokens.push(Token::Op(c.to_string())); } }
+            '(' => { if let Some(_) = chars.next() { tokens.push(Token::OpenParen) } }
+            ')' => { if let Some(_) = chars.next() { tokens.push(Token::ClosedParen) } }
             '*' => {
                 chars.next();
                 let star = chars.peek() == Some(&'*');
@@ -51,6 +51,18 @@ struct Node {
 }
 
 fn ast(tokens: &[Token]) -> ValOrExp {
+    let wrapped_in_parens = |tokens: &[Token]| -> bool {
+        tokens.len() > 2 && tokens[0] == Token::OpenParen && tokens[tokens.len() - 1] == Token::ClosedParen
+    };
+
+    fn strip_parens(tokens: &[Token]) -> &[Token] {
+        let mut tokens = tokens;
+        while let (Some(_), Some(_)) = (tokens.strip_prefix(&[Token::OpenParen]), tokens.strip_suffix(&[Token::ClosedParen])) {
+            tokens = &tokens[1..tokens.len() - 1];
+        }
+        tokens
+    }
+
     let parse_tokens_as_exp = |op_idx: usize| {
         let Token::Op(op) = tokens[op_idx].clone() else { unreachable!(); };
 
@@ -72,13 +84,40 @@ fn ast(tokens: &[Token]) -> ValOrExp {
                 let Token::Literal(num) = tokens[0].clone() else { unreachable!(); };
                 ValOrExp::Val(num)
             },
-            _ => panic!("This list of tokens does not contain a single operator, those it should only contain a single token (or noting) but found {tokens:?}"),
+            _ => {
+                if wrapped_in_parens(tokens) { return ast(strip_parens(tokens)); }
+                panic!("\n{}{}{}{}\n",
+                    "Failed to parse tokens into the ast.\n",
+                    "The list of tokens does not contain any operators,\n",
+                    "those it should only contain a single token, tokens wrapped in parens or noting\n",
+                    format!("Found {tokens:?}")
+                );
+            },
         }
     };
 
+    let closed_paren_idx = || {
+        if tokens[0] == Token::OpenParen {
+            let mut count = 0;
+            for (i, token) in tokens.iter().enumerate() {
+                match token {
+                    Token::OpenParen => count += 1,
+                    Token::ClosedParen => count -= 1,
+                    _ => {}
+                }
+                if count == 0 { return i; }
+            }
+        }
+
+        0
+    };
+
+    let skip = closed_paren_idx();
     let is_op = |t: &Token| if let Token::Op(_) = t { true } else { false };
-    match tokens.iter().position(is_op) {
-        Some(op_idx) => parse_tokens_as_exp(op_idx),
+    let op_pos = tokens.iter().skip(skip).position(is_op);
+
+    match op_pos {
+        Some(op_idx) => parse_tokens_as_exp(op_idx + skip),
         None => parse_tokens_as_val(),
     }
 }
@@ -108,7 +147,7 @@ fn eval(node: &ValOrExp) -> i64 {
 }
 
 fn main() {
-    let s = "2 + 2 * 2";
+    let s = "(1+2)*(3+4)";
     let tokens = tokenize(s);
     println!("{:?}", tokens);
     let ast = ast(&tokens);
