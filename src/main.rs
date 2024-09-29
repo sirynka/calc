493c6de1c1d3fd1517f0f0865f97464b3bf031d4
ssamaruk <ssamaruk@magnite.com>
2024-09-29 12:04:34 +0300

    incorporate lines into ast

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,9 @@ enum Token {
     Variable(String),
     Op(String),
     OpenParen,
-    ClosedParen
+    ClosedParen,
+    Equal,
+    EndOfLine,
 }
 
 fn tokenize(s: &str) -> Vec<Token> {
@@ -28,6 +30,8 @@ fn tokenize(s: &str) -> Vec<Token> {
             '+' | '-' | '/' | '%' => { if let Some(c) = chars.next() { tokens.push(Token::Op(c.to_string())); } }
             '(' => { if let Some(_) = chars.next() { tokens.push(Token::OpenParen) } }
             ')' => { if let Some(_) = chars.next() { tokens.push(Token::ClosedParen) } }
+            '=' => { if let Some(_) = chars.next() { tokens.push(Token::Equal) } }
+            '\n' | ';' => { if let Some(_) = chars.next() { tokens.push(Token::EndOfLine) } }
             '*' => {
                 chars.next();
                 let star = chars.peek() == Some(&'*');
@@ -41,36 +45,76 @@ fn tokenize(s: &str) -> Vec<Token> {
         }
     }
 
+    if tokens.first() != Some(&Token::EndOfLine) {
+        tokens.insert(0, Token::EndOfLine);
+    }
+
+    if tokens.last() != Some(&Token::EndOfLine) {
+        tokens.push(Token::EndOfLine);
+    }
+
     tokens
 }
 
+enum AST {
+    Lines(Vec<Line>),
+}
+
+enum Line {
+    Statement(Statement),
+    Expression(ExpressionLike),
+}
+
 #[derive(Debug, PartialEq)]
-enum ValOrExp {
+enum ExpressionLike {
     Var(String),
     Val(String),
-    Exp(Box<Node>),
+    Exp(Box<BinaryOp>),
     Empty,
 }
 
-impl std::fmt::Display for ValOrExp {
+#[derive(Debug, PartialEq)]
+struct BinaryOp {
+    op: String,
+    left: ExpressionLike,
+    right: ExpressionLike,
+}
+
+struct Statement {
+    var: String,
+    exp: ExpressionLike,
+}
+
+impl std::fmt::Display for ExpressionLike {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            ValOrExp::Empty => write!(f, "0"),
-            ValOrExp::Var(var) => write!(f, "{var}"),
-            ValOrExp::Val(num) => write!(f, "{num}"),
-            ValOrExp::Exp(node) => write!(f, "({} {} {})", node.left, node.op, node.right),
+            ExpressionLike::Empty => write!(f, "0"),
+            ExpressionLike::Var(var) => write!(f, "{var}"),
+            ExpressionLike::Val(num) => write!(f, "{num}"),
+            ExpressionLike::Exp(node) => write!(f, "({} {} {})", node.left, node.op, node.right),
         }
     }
 }
 
-#[derive(Debug, PartialEq)]
-struct Node {
-    op: String,
-    left: ValOrExp,
-    right: ValOrExp,
+impl std::fmt::Display for AST {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match self {
+            AST::Lines(lines) => {
+                let lines = lines.iter().enumerate().map(|(i, line)| (line, i == 0));
+                for (line, first) in lines {
+                    if !first { writeln!(f)?; }
+                    match line {
+                        Line::Statement(stmt) => write!(f, "{} = {}", stmt.var, stmt.exp)?,
+                        Line::Expression(exp) => write!(f, "{}", exp)?,
+                    };
+                }
+                Ok(())
+            }
+        }
+    }
 }
 
-fn ast(tokens: &[Token]) -> ValOrExp {
+fn parse_exp(tokens: &[Token]) -> ExpressionLike {
     let op_precedence = ["-", "+", "*", "/", "%", "**"];
 
     let wrapped_in_parens = |tokens: &[Token]| -> bool {
@@ -88,12 +132,12 @@ fn ast(tokens: &[Token]) -> ValOrExp {
     let parse_tokens_as_exp = |op_idx: usize| {
         let Token::Op(op) = tokens[op_idx].clone() else { unreachable!(); };
 
-        ValOrExp::Exp(
+        ExpressionLike::Exp(
             Box::new(
-                Node {
+                BinaryOp {
                     op,
-                    left: ast(&tokens[..op_idx]),
-                    right: ast(&tokens[op_idx + 1..]),
+                    left: parse_exp(&tokens[..op_idx]),
+                    right: parse_exp(&tokens[op_idx + 1..]),
                 }
             )
         )
@@ -101,16 +145,16 @@ fn ast(tokens: &[Token]) -> ValOrExp {
 
     let parse_tokens_as_val = || {
         match tokens.len() {
-            0 => return ValOrExp::Empty,
+            0 => return ExpressionLike::Empty,
             1 => return {
                 match &tokens[0] {
-                    Token::Literal(num) => ValOrExp::Val(num.clone()),
-                    Token::Variable(var) => ValOrExp::Var(var.clone()),
+                    Token::Literal(num) => ExpressionLike::Val(num.clone()),
+                    Token::Variable(var) => ExpressionLike::Var(var.clone()),
                     _ => unreachable!(),
                 }
             },
             _ => {
-                if wrapped_in_parens(tokens) { return ast(strip_parens(tokens)); }
+                if wrapped_in_parens(tokens) { return parse_exp(strip_parens(tokens)); }
                 panic!("\n{}{}{}{}\n",
                     "Failed to parse tokens into the ast.\n",
                     "The list of tokens does not contain any operators,\n",
@@ -150,17 +194,56 @@ fn ast(tokens: &[Token]) -> ValOrExp {
     }
 }
 
+fn parse_stmt(tokens: &[Token]) -> Statement {
+    if tokens[1] != Token::Equal {
+        panic!("{}\n{}",
+            "Statements can not contain anything but '=' as tok[1]",
+            format!("Found {tokens:?}")
+        );
+    }
+
+    let Token::Variable(var) = &tokens[0] else {
+        panic!("Cannot assign {:?} to {:?}", tokens[0], &tokens[2..]);
+    };
+
+    Statement {
+        var: var.clone(),
+        exp: parse_exp(&tokens[2..]),
+    }
+}
+
+fn parse_lines(tokens: &[Token]) -> AST {
+    let mut eol_pos: Vec<usize> = Vec::new();
+    for (i, token) in tokens.iter().enumerate() {
+        if let Token::EndOfLine = token { eol_pos.push(i); }
+    }
+
+    let mut lines: Vec<Line> = Vec::new();
+    for w in eol_pos.windows(2) {
+        if let [b, e] = w {
+            let line = &tokens[*b+1..*e];
+            if line.is_empty() { continue; }
+            match line.contains(&Token::Equal) {
+                true => lines.push(Line::Statement(parse_stmt(line))),
+                false => lines.push(Line::Expression(parse_exp(line))),
+            }
+        }
+    }
+
+    AST::Lines(lines)
+}
+
 fn pow(base: i64, exp: i64) -> i64 {
     let mut res = 1;
     for _ in 0..exp { res *= base; }
     res
 }
 
-fn eval(node: &ValOrExp, stack: &Stack) -> i64 {
+fn eval(node: &ExpressionLike, stack: &Stack) -> i64 {
     match node {
-        ValOrExp::Empty => 0,
-        ValOrExp::Val(num) => num.parse().unwrap(),
-        ValOrExp::Exp(node) => {
+        ExpressionLike::Empty => 0,
+        ExpressionLike::Val(num) => num.parse().unwrap(),
+        ExpressionLike::Exp(node) => {
             match node.op.as_str() {
                 "+" => eval(&node.left, stack) + eval(&node.right, stack),
                 "-" => eval(&node.left, stack) - eval(&node.right, stack),
@@ -171,7 +254,7 @@ fn eval(node: &ValOrExp, stack: &Stack) -> i64 {
                 _ => panic!("Unknown operator({})", node.op),
             }
         }
-        ValOrExp::Var(var) => {
+        ExpressionLike::Var(var) => {
             match stack.get(var.as_str()) {
                 Some(value) => *value,
                 None => panic!("Var({var}) was not on a Stack\n{stack:?}"),
@@ -180,30 +263,18 @@ fn eval(node: &ValOrExp, stack: &Stack) -> i64 {
     }
 }
 
-fn execute(program: &str) {
+fn exec(ast: &AST) {
     let mut stack = Stack::new();
-
-    let exec_exp = |exp: &str, stack: &Stack| -> i64 {
-        let tokens = tokenize(exp);
-        let ast = ast(&tokens);
-        let value = eval(&ast, &stack);
-        value
-    };
-
-    fn skip_empty(line: &str) -> Option<&str> {
-        let line = line.trim();
-        if line.is_empty() { None } else { Some(line) }
-    }
-
-    for line in program.lines().filter_map(skip_empty) {
-        match line.split_once("=") {
-            Some((var, exp)) => {
-                let value = exec_exp(exp, &stack);
-                stack.insert(var.trim(), value);
+    let AST::Lines(lines) = ast;
+    for line in lines {
+        match line {
+            Line::Statement(stmt) => {
+                let value = eval(&stmt.exp, &stack);
+                stack.insert(stmt.var.as_str(), value);
             }
-            None => {
-                let value = exec_exp(line, &stack);
-                println!("{}", value);
+            Line::Expression(exp) => {
+                let value = eval(exp, &stack);
+                println!("{value}");
             }
         }
     }
@@ -211,49 +282,54 @@ fn execute(program: &str) {
 
 fn main() {
     let program = "
-        a = 1
-        b = 1
+    a = 1
+    b = 1
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
 
-        a
-        tmp = a + b
-        a = b
-        b = tmp
+    a
+    tmp = a + b
+    a = b
+    b = tmp
     ";
 
-    execute(program);
+    let tokens = tokenize(&program);
+    let ast = parse_lines(&tokens);
+    // println!("{tokens:?}");
+    // println!("{ast}");
+
+    exec(&ast);
 }
